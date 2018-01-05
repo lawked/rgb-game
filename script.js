fb1ddffcb5377750ba8b8d3af18b67e9802320b1
Waldemar Dubiel <waldemar.dubiel@performgroup.com>
2018-01-05 21:13:45 +0100

    added game over functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let score = 0;
-let lifes = 3;
+let score;
+let lifes;
 let rgbsArray;
 let masterColor;
 let scoreElement;
@@ -9,94 +9,129 @@ let colorBoxes;
 let masterColorElement;
 let lifesField;
 
-function getElement(cssSelector) {
-    return document.querySelector(cssSelector);
+let getElement = (cssSelector) => {
+  return document.querySelector(cssSelector);
 }
 
-function generateRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+let generateRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function getRandomRgb() {
-    let r = generateRandomInt(0, 255);
-    let g = generateRandomInt(0, 255);
-    let b = generateRandomInt(0, 255);
-    return ("(" + r + ", " + g + ", " + b + ")");
+let getRandomRgb = () => {
+  let r = generateRandomInt(0, 255);
+  let g = generateRandomInt(0, 255);
+  let b = generateRandomInt(0, 255);
+  return ("(" + r + ", " + g + ", " + b + ")");
 }
 
-function getRandomRgbsArray() {
-    let rgbs = [];
-    for (let i = 0; i < 9; i++) {
-        rgbs.push("rgb" + getRandomRgb());
-    }
-    return rgbs;
+let getRandomRgbsArray = () => {
+  let rgbs = [];
+  for (let i = 0; i < 9; i++) {
+    rgbs.push("rgb" + getRandomRgb());
+  }
+  return rgbs;
 }
 
-function shuffleOneRgb(rgbsArray) {
-    return rgbsArray[generateRandomInt(0, 8)];
+let shuffleOneRgb = (rgbsArray) => {
+  return rgbsArray[generateRandomInt(0, 8)];
 }
 
-function setBoxesColors(colors) {
-    colorBoxes = document.querySelectorAll(".square");
-    for (let i = 0; i < 9; i++) {
-        colorBoxes[i].style.backgroundColor = colors[i];
-    }
+let setBoxesColors = (colors) => {
+  colorBoxes = document.querySelectorAll(".square");
+  for (let i = 0; i < 9; i++) {
+    colorBoxes[i].style.backgroundColor = colors[i];
+  }
 }
 
-function setMasterColor(colors) {
-    masterColor = shuffleOneRgb(colors);
-    masterColorElement = getElement(".result-item.shuffled-rgb");
-    masterColorElement.style.backgroundColor = masterColor;
-    masterColorElement.innerHTML = masterColor;
+let setMasterColor = (colors) => {
+  masterColor = shuffleOneRgb(colors);
+  masterColorElement = getElement(".result-item.shuffled-rgb");
+  masterColorElement.style.backgroundColor = masterColor;
+  masterColorElement.innerHTML = masterColor;
 }
 
-function init() {
-    lifesField = getElement(".result-item.lifes span");
-    lifesField.innerHTML = lifes;
-    scoreElement = getElement(".result-item.score span");
-    scoreElement.innerHTML = score;
-    rgbsArray = getRandomRgbsArray()
+let init = () => {
+  score = 0;
+  lifes = 3;
+  lifesField = getElement(".result-item.lifes span");
+  lifesField.innerHTML = lifes;
+  scoreElement = getElement(".result-item.score span");
+  scoreElement.innerHTML = score;
+  rgbsArray = getRandomRgbsArray()
 }
 
-function setUpListener() {
-    for (let i = 0; i < 9; i++) {
-        colorBoxes[i].addEventListener("click", verifyUserChoice);
-    }
+let setUpColorListener = () => {
+  for (let i = 0; i < 9; i++) {
+    colorBoxes[i].addEventListener("click", verifyUserChoice);
+  }
 }
 
-function verifyUserChoice(event) {
+let verifyUserChoice = (event) => {
+  if (lifes > 1) {
     if (event.target.style.backgroundColor === masterColor) {
-        for (let j = 0; j < 9; j++) {
-            colorBoxes[j].style.backgroundColor = masterColor;
-        }
-        increaseScore();
+      increaseScore();
+      if (score==10){
+        increaseLifes();
+      }
     } else {
-        decreaseLifes();
-        decreaseScore();
+      decreaseLifes();
+      decreaseScore();
     }
     rgbsArray = getRandomRgbsArray();
     setMasterColor(rgbsArray);
     setBoxesColors(rgbsArray);
+  } else {
+    decreaseLifes();
+    toogleGameOverMessage(true);
+  }
 }
 
-function decreaseScore() {
+let decreaseScore = () => {
+  if (score>0){
     scoreElement.innerHTML = --score;
+  }
 }
 
-function increaseScore() {
-    scoreElement.innerHTML = ++score;
+let increaseScore = () => {
+  scoreElement.innerHTML = ++score;
 }
 
-function decreaseLifes() {
-    lifesField.innerHTML = --lifes;
+let decreaseLifes = () => {
+  lifesField.innerHTML = --lifes;
 }
 
-function dincreaseLifes() {
-    lifesField.innerHTML = ++lifes;
+let increaseLifes = () => {
+  lifesField.innerHTML = ++lifes;
 }
 
-init();
-setMasterColor(rgbsArray);
-setBoxesColors(rgbsArray);
-setUpListener();
+let toogleGameOverMessage = (display) => {
+  let gameOverElement = document.querySelector("#game-over");
+  if (display) {
+    gameOverElement.style["display"] = "block";
+  }
+  else {
+    gameOverElement.style["display"] = "none";
+  }
+}
+
+let setUpResetListener = () => {
+  for (let i = 0; i < 9; i++) {
+    document.querySelector('#play-again button').addEventListener("click", resetAllGame);
+  }
+}
+
+let resetAllGame = () => {
+  setUpAll();
+  toogleGameOverMessage(false);
+}
+
+let setUpAll = () => {
+  init();
+  setMasterColor(rgbsArray);
+  setBoxesColors(rgbsArray);
+  setUpColorListener();
+  setUpResetListener();
+}
+
+setUpAll();
 
